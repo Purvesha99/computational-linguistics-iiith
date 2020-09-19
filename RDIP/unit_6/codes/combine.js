9533db9a746e53af48dc2f3d4015d37b6a1cab76
Purvesha99 <68986783+Purvesha99@users.noreply.github.com>
2020-09-19 21:07:32 +0530

    Update combine.js

--- RDIP/unit_6/codes/combine.js
+++ RDIP/unit_6/codes/combine.js
@@ -1,8 +1,22 @@
+function select(sel){
+			var corp_id=corp.options[corp.selectedIndex].value;
+			if(corp_id=="null"){
+				alert("Select a corpus");
+				return false;
+			}
+			if(corp_id=="corp1"){
+				select2e.style.display="block";				
+			}
+			if(corp_id=="corp2"){
+				select2h.style.display="block";
+			}
+		}
+
 function check(){
-	if(){
-		document.getElementById("ri_wr").innerHTML="<img src="right.png">";
+	if(a==ans){
+		document.getElementById("ri_wr").innerHTML="<img src=\"right.png\">";
 	}
 	else{
-		document.getElementById("ri_wr").innerHTML="<img src="right.png">";
+		document.getElementById("ri_wr").innerHTML="<img src=\"right.png\">";
 	}
 }
